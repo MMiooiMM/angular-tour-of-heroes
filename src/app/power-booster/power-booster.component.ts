b7ef4b26885a621e6a51b557592baabda05ca154
黃郁倫 mio <mio@miniasp.com>
2019-09-02 15:01:52 +0800

    move service

--- src/app/power-booster/power-booster.component.ts
+++ src/app/power-booster/power-booster.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit, Input } from '@angular/core';
 import { Hero } from '../hero';
 import { ActivatedRoute } from '@angular/router';
-import { HeroService } from '../hero.service';
+import { HeroService } from '../service/hero.service';
 import { Location } from '@angular/common';
 
 @Component({
